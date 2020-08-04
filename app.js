ad10c46d77af1ebb1d18a075c012b0a9dc199bef
Kevin <mkevinhall@gmail.com>
2020-08-04 08:41:04 -0400

    updated files

--- app.js
+++ app.js
@@ -1,47 +1,63 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('grids.html');
 
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
 
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public/')
+app.use(express.static(publicDirectoryPath))
 
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + './consent.html'));
 });
 
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
+app.listen(3000, () => {
+    console.log("hello log");
+})
 
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+// var port = process.env.PORT || 3000,
+//     http = require('http'),
+//     fs = require('fs'),
+//     html = fs.readFileSync('grids.html');
+//
+// var log = function(entry) {
+//     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+// };
+//
+// var server = http.createServer(function (req, res) {
+//     if (req.method === 'POST') {
+//         var body = '';
+//
+//         req.on('data', function(chunk) {
+//             body += chunk;
+//         });
+//
+//         req.on('end', function() {
+//             if (req.url === '/') {
+//                 log('Received message: ' + body);
+//             } else if (req.url = '/scheduled') {
+//                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+//             }
+//
+//             res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+//             res.end();
+//         });
+//     } else {
+//         res.writeHead(200);
+//         res.write(html);
+//         res.end();
+//     }
+// });
+
+// // Listen on port 3000, IP defaults to 127.0.0.1
+// server.listen(port);
+//
+// // Put a friendly message on the terminal
+// console.log('Server running at http://127.0.0.1:' + port + '/');
 
 
 
 // const express = require('express');
-// const path = require('path');
 // const app = express();
 //
 //
