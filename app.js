570aa35d1ba23684b7a5aea404859bb9e47abe88
Kevin <mkevinhall@gmail.com>
2020-08-04 06:05:48 -0400

    added files

--- app.js
+++ app.js
@@ -18,16 +18,16 @@ var consent = require('./public/consent');
 //app.use(express.static(path.join(__dirname, '/')));
 //app.use('/public', express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/public/consent.html'));
-// });
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/consent.html'));
+});
 
-exports.index = function(req, res) {
- res.render('consent', {title: 'consent page'});
-};
-
-exports.add_consent = function(req, res) {
-};
+// exports.index = function(req, res) {
+//  res.render('consent', {title: 'consent page'});
+// };
+//
+// exports.add_consent = function(req, res) {
+// };
 
 app.get('/consent', consent.index);
 app.post('/add_consent', consent.add_hike);
