9cd331e6ec56ee6eeb399899decff8052a0260f0
Kevin <mkevinhall@gmail.com>
2020-08-04 05:36:13 -0400

    express research gridstudy

---
+++ app.js
@@ -0,0 +1,88 @@
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+// -- public keys
+
+var bucketName = "BUCKET_NAME";
+var bucketRegion = "REGION";
+var IdentityPoolId = "IDENTITY_POOL_ID";
+
+//app.use(express.static('public'));
+
+//app.use(express.static(path.join(__dirname, '/')));
+//app.use('/public', express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/grids.html'));
+});
+
+app.listen(3000, () => {
+    console.log("hello log");
+})
+
+
+// var AWS = require('aws-sdk');
+// var s3 = new AWS.S3();
+//
+// var credentials = new AWS.SharedIniFileCredentials({profile: '....'});
+// AWS.config.credentials = credentials;
+//
+//
+// var params = {
+//     Bucket: 'gridstudy',
+//     Key: 'output.csv',
+//     Body: csvFileContent,
+//     ContentType: 'application/octet-stream',
+//     ContentDisposition: contentDisposition(filePath, {
+//         type: 'inline'
+//     }),
+//     CacheControl: 'public, max-age=86400'
+// }
+
+
+// s3.putObject(params, function(err, data) {
+//     if (err) {
+//         console.log("Error at uploadCSVFileOnS3Bucket function", err);
+//         next(err);
+//     } else {
+//         console.log("File uploaded Successfully");
+//         next(null, filePath);
+//     }
+// });
+
+// function addCsv(csvName) {
+//   var files = document.getElementById("").files;
+//   if (!files.length) {
+//     return alert("Please choose a file to upload first.");
+//   }
+//   var file = files[0];
+//   var fileName = file.name;
+//   var csvKey = encodeURIComponent(csvName) + "/";
+//   var bucketName = "gridstudy"
+//
+//   // Use S3 ManagedUpload class as it supports multipart uploads
+//   var upload = new AWS.S3.ManagedUpload({
+//     params: {
+//       Bucket: bucketName,
+//       Key: csvKey,
+//       Body: file,
+//       ACL: "public-read"
+//     }
+//   });
+//
+//   var promise = upload.promise();
+//
+//   promise.then(
+//     function(data) {
+//       alert("Successfully uploaded csv file.");
+//       viewAlbum(albumName);
+//     },
+//     function(err) {
+//       return alert("There was an error uploading the file: ", err.message);
+//     }
+//   );
+// }
