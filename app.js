b7e5c159a564f019b8c9748108b1dcbfa634333f
Kevin <mkevinhall@gmail.com>
2020-08-04 09:23:41 -0400

    updated files

--- app.js
+++ app.js
@@ -1,97 +1,99 @@
 
+//
+//
+// // Include the cluster module
+// var cluster = require('cluster');
+//
+// // Code to run if we're in the master process
+// if (cluster.isMaster) {
+//
+//     // Count the machine's CPUs
+//     var cpuCount = require('os').cpus().length;
+//
+//     // Create a worker for each CPU
+//     for (var i = 0; i < cpuCount; i += 1) {
+//         cluster.fork();
+//     }
+//
+//     // Listen for terminating workers
+//     cluster.on('exit', function (worker) {
+//
+//         // Replace the terminated workers
+//         console.log('Worker ' + worker.id + ' died :(');
+//         cluster.fork();
+//
+//     });
+//
+// // Code to run if we're in a worker process
+// } else {
+//     var AWS = require('aws-sdk');
+//     var path = require('path');
+//     var express = require('express');
+//
+//     AWS.config.region = process.env.REGION
+//
+//     var sns = new AWS.SNS();
+//     var ddb = new AWS.DynamoDB();
+//
+//     var ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
+//     var snsTopic =  process.env.NEW_SIGNUP_TOPIC;
+//     var app = express();
+//
+//     app.use(express.static(__dirname + '/public'));
+//
+//     app.get('/', function(req, res) {
+//       res.sendFile(path.join(__dirname + './consent.html'));
+//     });
+//
+//     var port = process.env.PORT || 3000;
+//
+//     var server = app.listen(port, function () {
+//         console.log('Server running at http://127.0.0.1:' + port + '/');
+//     });
+// }
 
+var AWS = require('aws-sdk');
 
-// Include the cluster module
-var cluster = require('cluster');
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('consent.html');
 
-// Code to run if we're in the master process
-if (cluster.isMaster) {
+var log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
 
-    // Count the machine's CPUs
-    var cpuCount = require('os').cpus().length;
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
 
-    // Create a worker for each CPU
-    for (var i = 0; i < cpuCount; i += 1) {
-        cluster.fork();
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
     }
+});
 
-    // Listen for terminating workers
-    cluster.on('exit', function (worker) {
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
 
-        // Replace the terminated workers
-        console.log('Worker ' + worker.id + ' died :(');
-        cluster.fork();
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
 
-    });
-
-// Code to run if we're in a worker process
-} else {
-    var AWS = require('aws-sdk');
-    var path = require('path');
-    var express = require('express');
-
-    AWS.config.region = process.env.REGION
-
-    var sns = new AWS.SNS();
-    var ddb = new AWS.DynamoDB();
-
-    var ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
-    var snsTopic =  process.env.NEW_SIGNUP_TOPIC;
-    var app = express();
-
-    app.use(express.static(__dirname + '/public'));
-
-    app.get('/', function(req, res) {
-      res.sendFile(path.join(__dirname + './consent.html'));
-    });
-
-    // app.post('/signup', function(req, res) {
-    //     var item = {
-    //         'email': {'S': req.body.email},
-    //         'name': {'S': req.body.name},
-    //         'preview': {'S': req.body.previewAccess},
-    //         'theme': {'S': req.body.theme}
-    //     };
-    //
-    //     ddb.putItem({
-    //         'TableName': ddbTable,
-    //         'Item': item,
-    //         'Expected': { email: { Exists: false } }
-    //     }, function(err, data) {
-    //         if (err) {
-    //             var returnStatus = 500;
-    //
-    //             if (err.code === 'ConditionalCheckFailedException') {
-    //                 returnStatus = 409;
-    //             }
-    //
-    //             res.status(returnStatus).end();
-    //             console.log('DDB Error: ' + err);
-    //         } else {
-    //             sns.publish({
-    //                 'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email
-    //                                     + "\r\nPreviewAccess: " + req.body.previewAccess
-    //                                     + "\r\nTheme: " + req.body.theme,
-    //                 'Subject': 'New user sign up!!!',
-    //                 'TopicArn': snsTopic
-    //             }, function(err, data) {
-    //                 if (err) {
-    //                     res.status(500).end();
-    //                     console.log('SNS Error: ' + err);
-    //                 } else {
-    //                     res.status(201).end();
-    //                 }
-    //             });
-    //         }
-    //     });
-    // });
-
-    var port = process.env.PORT || 3000;
-
-    var server = app.listen(port, function () {
-        console.log('Server running at http://127.0.0.1:' + port + '/');
-    });
-}
 
 // const express = require('express');
 // const path = require('path');
