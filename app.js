0642d0d7ad7ee040655cde563f2844d32a416bb2
Kevin <mkevinhall@gmail.com>
2020-08-04 08:27:05 -0400

    updated files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
-    html = fs.readFileSync('consent.html');
+    html = fs.readFileSync('grids.html');
 
 var log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
