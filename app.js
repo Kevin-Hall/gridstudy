59886b46c5acdc9dd003ac033e7dc9ceadfe40c2
Kevin <mkevinhall@gmail.com>
2020-08-04 09:41:16 -0400

    updated files

--- app.js
+++ app.js
@@ -46,7 +46,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public/')
+const publicDirectoryPath = path.join(__dirname, 'public')
 app.use(express.static(publicDirectoryPath))
 
 app.get('/', function(req, res){
