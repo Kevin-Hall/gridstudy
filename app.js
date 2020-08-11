d8be2935f63b0e2d417153185e37683459e66e88
Kevin <mkevinhall@gmail.com>
2020-08-11 04:31:03 -0400

    updated files

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/" + "consent.html" );
+   res.sendFile( __dirname + "/" + "index.html" );
 })
 
 // error handler
