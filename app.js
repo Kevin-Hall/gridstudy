4bb88ec64a76e81932fb9c5beb639bcecb589147
Kevin <mkevinhall@gmail.com>
2020-08-11 04:54:23 -0400

    updated files

--- app.js
+++ app.js
@@ -23,18 +23,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({ secret: 'our new secret'}));
 
-// app.use('/', indexRouter);
-// app.use('/introduction', introRouter);
+app.use('/', indexRouter);
+app.use('/introduction', introRouter);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
 // });
 
-app.get('/', function (req, res) {
-   res.sendFile( "./index.html");
-})
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
 
+app.listen(3000);
 // error handler
 // app.use(function(err, req, res, next) {
 //   // set locals, only providing error in development
