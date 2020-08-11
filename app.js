394b87383176cb9fd144208d101ae3580e4223ac
Kevin <mkevinhall@gmail.com>
2020-08-11 06:02:05 -0400

    testing requests

--- app.js
+++ app.js
@@ -10,26 +10,21 @@ var port = process.env.PORT || 3000,
 
 var server = http.createServer(function (req, res) {
     if (req.method === 'POST') {
-        var body = '';
-
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
+        // var body = '';
+        //
+        // req.on('data', function(chunk) {
+        //     body += chunk;
+        // });
+        //
+        // req.on('end', function() {
+        //     res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+        //     res.end();
+        // });
         res.write(html);
         res.end();
+    } else {
+        //res.writeHead(200);
+
     }
 });
 
