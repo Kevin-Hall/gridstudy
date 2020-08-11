08a0a18b69d983014ad2f9dde8ddef3043108213
Kevin <mkevinhall@gmail.com>
2020-08-11 05:21:39 -0400

    updated files

--- app.js
+++ app.js
@@ -1,44 +1,5 @@
 
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
 
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
-
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
-
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {"Content-Type": "text/html; charset=utf-8"});
-            res.end();
-        });
-    } else {
-        res.set('Content-Type', 'text/javascript')
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
-});
-
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
-
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
 // var createError = require('http-errors');
 // var express = require('express');
 // var path = require('path');
@@ -133,19 +94,19 @@ console.log('Server running at http://127.0.0.1:' + port + '/');
 // console.log('Server running at http://127.0.0.1:' + port + '/');
 
 
-// const express = require('express');
-// const path = require('path');
-// var consent = require('./routes/hike');
-// const app = express();
+const express = require('express');
+const path = require('path');
+//var consent = require('./routes/hike');
+const app = express();
 //
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.get('/', function (req, res) {
-//    res.sendFile( __dirname + "/" + "consent.html" );
-// })
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
 //
-// app.listen(3000, () => {
-//     console.log('Server running at http://127.0.0.1:3000/');
-// })
+app.listen(3000, () => {
+    console.log('Server running at http://127.0.0.1:3000/');
+})
 
 // var port = process.env.PORT || 3000,
 //     http = require('http'),
