0f5b1bfb6a71d87813ee6af4de5e6bf63e56f269
Kevin <mkevinhall@gmail.com>
2020-08-11 09:01:28 -0400

    testing bucket upload

--- public/index.js
+++ public/index.js
@@ -298,19 +298,18 @@ function takeBreak(e) {
   const fileName = 'Eto.csv';
 
   const uploadFile = () => {
-    fs.readFile(fileName, (err, data) => {
-       if (err) throw err;
-       const params = {
-           Bucket: 'gridstudy', // pass your bucket name
-           Key: 'test2.csv', // file will be saved as testBucket/contacts.csv
-           Body: arrayToCSV(comparisons)
-       };
-       s3.upload(params, function(s3Err, data) {
-           if (s3Err) throw s3Err
-           console.log(`File uploaded successfully at ${data.Location}`)
-       });
+    const params = {
+        Bucket: 'gridstudy', // pass your bucket name
+        Key: 'test2.csv', // file will be saved as testBucket/contacts.csv
+        Body: arrayToCSV(comparisons)
+    };
+    s3.upload(params, function(s3Err, data) {
+        if (s3Err) throw s3Err
+        console.log(`File uploaded successfully at ${data.Location}`)
     });
   };
+
+  uploadFile();
 
   document.getElementById("timer").style.display = "block";
   document.getElementById("left").style.display = "none";
