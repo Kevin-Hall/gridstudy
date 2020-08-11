c4cf0f9f5a579dd8d11130e5bc668531585ad7fa
Kevin <mkevinhall@gmail.com>
2020-08-11 09:13:04 -0400

    testing bucket upload

--- public/index.js
+++ public/index.js
@@ -301,7 +301,7 @@ function takeBreak(e) {
     const params = {
         Bucket: 'gridstudy', // pass your bucket name
         Key: 'test2.csv', // file will be saved as testBucket/contacts.csv
-        Body: arrayToCSV(comparisons)
+        Body: arrayToCSV(comparisons_test)
     };
     s3.upload(params, function(s3Err, data) {
         if (s3Err) throw s3Err
