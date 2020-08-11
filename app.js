32650513c023b882987603a5715fa97f41b6be32
Kevin <mkevinhall@gmail.com>
2020-08-11 06:21:15 -0400

    changed server var

--- app.js
+++ app.js
@@ -6,31 +6,38 @@ var port = process.env.PORT || 3000,
     html = fs.readFileSync('index.html');
 
 
-    var server = http.createServer(function (request, response) {
-        fs.readFile('./' + request.url, function(err, data) {
-            if (!err) {
-                var dotoffset = request.url.lastIndexOf('.');
-                var mimetype = dotoffset == -1
-                                ? 'text/plain'
-                                : {
-                                    '.html' : 'text/html',
-                                    '.ico' : 'image/x-icon',
-                                    '.jpg' : 'image/jpeg',
-                                    '.png' : 'image/png',
-                                    '.gif' : 'image/gif',
-                                    '.css' : 'text/css',
-                                    '.js' : 'text/javascript'
-                                    }[ request.url.substr(dotoffset) ];
-                response.setHeader('Content-type' , mimetype);
-                response.end(data);
-                console.log( request.url, mimetype );
-            } else {
-                console.log ('file not found: ' + request.url);
-                response.writeHead(404, "Not Found");
-                response.end();
-            }
-        });
-    })
+
+var server = http.createServer(function (request, response) {
+  if (req.method === 'POST') {
+    fs.readFile('./' + request.url, function(err, data) {
+        if (!err) {
+            var dotoffset = request.url.lastIndexOf('.');
+            var mimetype = dotoffset == -1
+                            ? 'text/plain'
+                            : {
+                                '.html' : 'text/html',
+                                '.ico' : 'image/x-icon',
+                                '.jpg' : 'image/jpeg',
+                                '.png' : 'image/png',
+                                '.gif' : 'image/gif',
+                                '.css' : 'text/css',
+                                '.js' : 'text/javascript'
+                                }[ request.url.substr(dotoffset) ];
+            response.setHeader('Content-type' , mimetype);
+            response.end(data);
+            console.log( request.url, mimetype );
+        } else {
+            console.log ('file not found: ' + request.url);
+            response.writeHead(404, "Not Found");
+            response.end();
+        }
+    });
+  } else {
+      res.writeHead(200);
+      res.write(html);
+      res.end();
+  }
+})
 
 
 // Listen on port 3000, IP defaults to 127.0.0.1
