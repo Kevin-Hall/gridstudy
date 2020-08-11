61483453573c05037afec6479a1671c4db5678e4
Kevin <mkevinhall@gmail.com>
2020-08-11 05:39:20 -0400

    basic config

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
+var AWS = require('aws-sdk');
+// const app = express();
+// app.use(express.static(__dirname + '/public'));
 
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
+
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
+});
+
+
+
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
 
 // var createError = require('http-errors');
 // var express = require('express');
@@ -156,8 +198,8 @@
 
 
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 //
 //
 // // -- public keys
@@ -172,25 +214,14 @@ const app = express();
 //app.use(express.static(path.join(__dirname, '/')));
 //app.use('/public', express.static(path.join(__dirname, "public")));
 //app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-// exports.index = function(req, res) {
-//  res.render('consent', {title: 'consent page'});
-// };
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
 //
-// exports.add_consent = function(req, res) {
-// };
-
-// app.get('/consent', consent.index);
-// app.post('/add_consent', consent.add_hike);
-
-
-app.listen(3000);
-, () => {
-    console.log("listening port 3000");
-})
+// app.listen(3000);
+// , () => {
+//     console.log("listening port 3000");
+// })
 
 
 
