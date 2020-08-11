f0e55519551a28318fd17529f87f914c2ec85e83
Kevin <mkevinhall@gmail.com>
2020-08-11 06:37:59 -0400

    switched to exp

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var app = express()
 
 app.use(express.static('public'));
 
+app.get('/', function(req,res) {
+  res.sendfile('index.html');
+});
+
 var server = app.listen(3000, function () {
 
     var host = server.address().address
