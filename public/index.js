36e8b353e150ecf954fb40be7469736a51313a3c
Kevin <mkevinhall@gmail.com>
2020-08-18 10:07:34 -0400

    testing upload

--- public/index.js
+++ public/index.js
@@ -373,7 +373,9 @@ function takeBreak(e) {
   formData.append('csv', blob);
 
   var file = new File([blob], "newcsv");
-  getSignedRequest(content);
+  //getSignedRequest(content);
+
+  uploadToS3(content);
 
 
   //
