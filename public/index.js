9ad82508a76c6793c9b1022b93b2f441069e7b2a
Kevin <mkevinhall@gmail.com>
2020-08-18 09:58:14 -0400

    testing screen sizes

--- public/index.js
+++ public/index.js
@@ -381,7 +381,9 @@ function takeBreak(e) {
   formData.append('csv', blob);
 
   var file = new File([blob], "newcsv");
-  getSignedRequest(content);
+  //getSignedRequest(content);
+
+  uploadToS3(content);
 
   //
   // setTimeout(() => {
