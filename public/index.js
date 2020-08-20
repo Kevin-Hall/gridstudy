3e366f2f431f133b16e016e14e1103e0a4bd8056
Kevin <mkevinhall@gmail.com>
2020-08-20 02:31:30 -0400

    testing upload

--- public/index.js
+++ public/index.js
@@ -391,20 +391,20 @@ function takeBreak(e) {
   getSignedRequest(file);
 
   var formData=new FormData();
-formData.append("uploadCsv",file);
-var request = new XMLHttpRequest();
+  formData.append("uploadCsv",file);
+  var request = new XMLHttpRequest();
 
- //here you can set the request header to set the content type, this can be avoided.
- //The browser sets the setRequestHeader and other headers by default based on the formData that is being passed in the request.
- request.setRequestHeader("Content-type", "multipart/form-data"); //----(*)
- request.open("POST","/handleFile", true);
-request.onreadystatechange = function (){
-    if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-    console.log("yey");
+   //here you can set the request header to set the content type, this can be avoided.
+   //The browser sets the setRequestHeader and other headers by default based on the formData that is being passed in the request.
+   request.setRequestHeader("Content-type", "multipart/form-data"); //----(*)
+   request.open("POST","/handleFile", true);
+   request.onreadystatechange = function (){
+      if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+      console.log("yey");
+      }
     }
-}
 
-request.send(formData);
+    request.send(formData);
 
 
 
