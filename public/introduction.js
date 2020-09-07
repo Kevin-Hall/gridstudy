69ddacdfe4b5784e83b9712b6b2a26d16440048b
Kevin <mkevinhall@gmail.com>
2020-09-07 03:22:08 -0400

    user info

--- public/introduction.js
+++ public/introduction.js
@@ -192,7 +192,7 @@ function begin_fast_practice(){
   document.getElementById("cross").style.display = "inline-block";
 
 
-  for(var i = 0;i <= 5;i++){
+  for(var i = 1;i >= 5;i++){
 
     var left = "FastExample" + i + "_A";
     var right = "FastExample" + i + "_B"
@@ -226,7 +226,7 @@ function next_icon_grids(){
   var eoi_text = document.getElementById("end_of_intruction_text");
 
 
-  if (index > 3){
+  if (index > 4){
     ldiv.style.display = "none";
     rdiv.style.display = "none";
     eoi_text.style.display = "block";
